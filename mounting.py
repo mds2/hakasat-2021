2c7814bfaa09fb3eaf6e9ceb32c7b8f7f873f4af
Michael Schuresko <michael.schuresko@gmail.com>
2021-06-26 21:36:11 -0700

    Have some progress on finding the bad sensor

--- mounting.py
+++ mounting.py
@@ -3,6 +3,21 @@ from pwn import *
 
 MAX_PANEL_POWER = 0.36878177829
 
+def directions():
+    for S in "+-":
+        for ax in "XYZ":
+            yield S + ax + "-Axis"
+
+def direction_map():
+    res = {}
+    for s in range(2):
+        for ax in range(3):
+            name = "+-"[s] + "XYZ"[ax] + "-Axis"
+            val = [0,0,0]
+            val[ax] = [1, -1][s]
+            res[name] = val
+    return res
+
 def open_up():
     r = remote("main-fleet.satellitesabove.me", 5005)
     r.recvuntil("Ticket please:\n")
@@ -44,6 +59,38 @@ def iterate_rot_mats():
                     r3 = list(np.cross(r1, r2))
                     yield np.array([r1, r2, r3])
 
+def tightness(results, skip):
+    l = []
+    for m,d in results:
+        for k in d:
+            if k != skip and d[k] > 0.0:
+                l.append(d[k])
+    v1 = min(l)
+    v2 = max(l)
+    v3 = v1
+    best_dist = 0.0
+    for x in l:
+        dist = min(abs(x - v1), abs(x - v2))
+        if dist > best_dist:
+            dist = best_dist
+            v3 = x
+    worst_score = 0.0
+    for x in l:
+        score = min(abs(x - v1), min(abs(x-v2), abs(x-v3)))
+        if score > worst_score:
+            worst_score = score
+    return worst_score
+        
+def find_odd_one_out(results):
+    d_out = None
+    best_tightness = 10.0
+    for d in directions():
+        t = tightness(results, d)
+        if t < best_tightness:
+            best_tightness = t
+            d_out = d
+    return d_out
+
 if __name__ == "__main__":
     r = open_up()
     results = []
@@ -51,5 +98,6 @@ if __name__ == "__main__":
         send_mat(r, m)
         r, vals = scoop_vals(r)
         results.append((m, vals))
-    print(results)
+    failing = find_odd_one_out(results)
+    print(failing)
 
